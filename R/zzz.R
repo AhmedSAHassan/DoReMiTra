5fa7496b1da2e92fb834e103e9b77deb01c18884
AhmedSAHassan <155752718+AhmedSAHassan@users.noreply.github.com>
2025-06-26 13:25:16 +0200

    correcting a typo

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  fl <- system.file("extdata", "metadata.tsv", package = pkgname)
+  fl <- system.file("extdata", "metadata.csv", package = pkgname)
   if (file.exists(fl)) {
     titles <- utils::read.delim(fl, stringsAsFactors = FALSE)$Title
     ExperimentHub::createHubAccessors(pkgname, titles)
