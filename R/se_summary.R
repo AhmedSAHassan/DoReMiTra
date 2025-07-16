ceddbc995404ba5d7c18642c074914538527af29
AhmedSAHassan <155752718+AhmedSAHassan@users.noreply.github.com>
2025-07-16 23:28:45 +0200

    modifying the function to print the message with the information needed for the summary

--- R/se_summary.R
+++ R/se_summary.R
@@ -18,36 +18,36 @@ se_summary <- function(dataset_name) {
   # Load metadata table
   metadata <- list_datasets()
 
-  if (!(dataset_name %in% metadata$DatasetName)) {
+  if (!(dataset_name %in% metadata$Dataset)) {
     stop("Dataset name '", dataset_name, "' not found in metadata.")
   }
 
   # Extract row for this dataset
 
-  meta_row <- metadata[metadata$DatasetName == dataset_name, ]
+  meta_row <- metadata[metadata$Dataset == dataset_name, ]
 
   # Load SummarizedExperiment object to get the number of samples included
 
-  se <- get_dataset(dataset_name)
-  metadata_file <- system.file("extdata", "metadata-DoReMiTra.csv", package = "DoReMiTra")
+  se <- get_radiation_data(dataset_name)
+  metadata_file <- read.csv(system.file("extdata", "metadata-DoReMiTra.csv", package = "DoReMiTra"))
 
   n_samples <- ncol(se)
   species <- meta_row$Organism
-  radiation <- meta_row$Radiation_type
-  setting <- meta_row$Exp_setting
-  accession <- metadata_file$Accession
-  link <- metadata_file$SourceUrl
+  radiation <- meta_row$RadiationType
+  setting <- meta_row$ExpSetting
+  accession <- metadata_file[metadata_file$Title == dataset_name, "Accession"]
+  link <- metadata_file[metadata_file$Title == dataset_name, "SourceUrl"]
 
-  message(glue::glue(
-    "\n Dataset: {dataset_name}",
-    "\n Organism(s): {species}",
-    "\n️  Radiation Type: {radiation}",
-    "\n Experiment Setting: {setting}",
-    "\n Number of Samples: {n_samples}",
-    "\n GEO Accession: {accession}",
-    "\n for more information about this study, please check {link}"
+
+print(glue::glue(
+    "\nDataset: {dataset_name}
+    Organism(s): {species}
+    Radiation Type: {radiation}
+    Experiment Setting: {setting}
+    Number of Samples: {n_samples}
+    Accession: {accession}
+    For more information about this study, please check {link}\n"
   ))
-
   # Return invisibly
 
   invisible(
@@ -58,7 +58,7 @@ se_summary <- function(dataset_name) {
       exp_setting = setting,
       accession = accession,
       n_samples = n_samples,
-      link <- meta_row$SourceUrl
+      link = meta_row$SourceUrl
     )
   )
 }
