77a0256f8d8353555ff7685d1e5cd5e5cbe1f37f
AhmedSAHassan <155752718+AhmedSAHassan@users.noreply.github.com>
2025-07-17 11:44:48 +0200

    adding load_doremitra_datasets() function

---
+++ R/load_doremitra_datasets.R
@@ -0,0 +1,33 @@
+
+
+#' Title
+#'
+#' @param verbose
+#'
+#' @returns
+#' all SummarizedExperiment objects and stores them in the global environment
+#' @export
+#'
+#' @examples
+#' load_doremitra_datasets()
+#'
+load_doremitra_datasets <- function(verbose = TRUE) {
+  eh <- ExperimentHub::ExperimentHub()
+  query_results <- query(eh, "DoReMiTra")
+
+  if (length(query_results) == 0) {
+    stop("No datasets found in ExperimentHub under 'DoReMiTra'.")
+  }
+
+  for (i in seq_along(query_results)) {
+    dataset_name <- query_results$title[i]
+
+    if (verbose) message("Loading dataset: ", dataset_name)
+
+    se_obj <- query_results[[i]]
+
+    # Assign to global environment
+    assign(dataset_name, se_obj, envir = .GlobalEnv)
+  }
+  invisible(NULL)
+}
