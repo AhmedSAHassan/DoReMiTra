2d6e500319ca7be382871809260a1012053f6224
AhmedSAHassan <155752718+AhmedSAHassan@users.noreply.github.com>
2025-08-04 22:02:11 +0200

    adding more description

--- R/DoReMiTra-pkg.R
+++ R/DoReMiTra-pkg.R
@@ -1,9 +1,11 @@
-#' DoReMiTra
+#' `DoReMiTra:` radiation DOse REsponse Measure In TRAnscriptomics
 #'
-#' `DoReMiTra` is a Bioconductor package that provides
-#'
-#' `DoReMiTra` addasdasdsadsa
+#' `DoReMiTra` is a Bioconductor package that provides curated and harmonized transcriptomic datasets
+#' from publicly available radiation studies. It enables easy access to
+#' dose- and time-dependent gene expression data across various organisms,
+#' radiation types, and experimental settings.
 #'
 #' @name DoReMiTra-pkg
+#' @docType package
 #' @keywords internal
 "_PACKAGE"
