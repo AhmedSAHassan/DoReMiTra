c341e93b38bde1be7302b8430fd23a31f4dcfb2c
AhmedSAHassan <155752718+AhmedSAHassan@users.noreply.github.com>
2025-08-12 13:59:25 +0200

    editing the function name

--- R/fetch_all_DoReMiTra.R
+++ R/fetch_all_DoReMiTra.R
@@ -15,7 +15,7 @@
 #' @examples
 #' get_all_DoReMiTra()
 #'
-get_all_DoReMiTra <- function(verbose = TRUE) {
+get_all_DoReMiTra_datasets <- function(verbose = TRUE) {
   # Retrieve all dataset names from metadata
   dataset_names <- list_DoReMiTra_datasets()$Dataset
 
