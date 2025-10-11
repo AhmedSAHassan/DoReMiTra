e3cc86cc2c48cafaa7063e023bf707b9cc63b1a8
AhmedSAHassan <155752718+AhmedSAHassan@users.noreply.github.com>
2025-10-12 01:21:00 +0200

    use the right test

--- tests/testthat/test-se_summary.R
+++ tests/testthat/test-se_summary.R
@@ -2,7 +2,7 @@ test_that("summarize_DoReMiTra_se rejects non-SummarizedExperiment", {
   expect_error(summarize_DoReMiTra_se("not_se"))
 
   se <- get_DoReMiTra_data("SE_Amundson_2008_ExVivo_GSE8917_GPL1708")
-  expect_output(summarize_DoReMiTra_se(se))
+  expect_message(summarize_DoReMiTra_se(se))
 
   se_strippedawaymeta <- se
   metadata(se_strippedawaymeta)[["DoReMiTra"]][["Radiation"]] <- NULL
